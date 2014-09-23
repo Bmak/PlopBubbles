142254094fa4d6ea068c8ad537b4d00037d45a53
Maxim <probmak@gmail.com>
2014-09-24 02:55:56 +0400

    start rep

---
+++ Assets/src/Ball.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class Ball : MonoBehaviour {
+
+	public Text tf;
+
+	int points = 10;
+	float speed;
+	bool isDead;
+
+	void Start () {
+		//tf = GameObject.FindGameObjectWithTag("Points").GetComponent("Text");
+
+
+		float scale = (float) Random.Range (3, 12)/10;
+		transform.localScale = new Vector2 (scale, scale);
+		gameObject.renderer.material.color = new Color(Random.value, Random.value, Random.value, 1);
+		gameObject.renderer.sortingLayerID = 0;
+
+
+		isDead = false;
+		speed = (float) Random.Range (1,5)/100;
+		speed /= scale;
+
+		points *= (int)scale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isDead) {
+			Vector3 pos = transform.position;
+			pos.y -= speed;
+			if (pos.y <= 0) {
+				isDead = true;
+			}
+			transform.position = pos;
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnMouseDown() {
+		//TODO add points
+		//int newPoints = int.Parse(tf.text) + points;
+		//tf.text = newPoints.ToString ();
+		isDead = true;
+	}
+
+	void Remove() {
+
+	}
+}
---
+++ Assets/src/GameControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+	
+	public GameObject pref;					//Префаб игрового объекта
+	public float skillK = 1;				//Коеффициент усложнения игры
+	public float minTimeBetweenSpawns;		//Минимальное значение между созданием нового объекта
+	public float maxTimeBetweenSpawns;		//Максимальное значение между созданием нового объекта
+	float _posY;
+	float _leftSpawnPosX;					// The x coordinate of position if it's instantiated on the left.
+	float _rightSpawnPosX;					// The x coordinate of position if it's instantiated on the right.
+
+	// Use this for initialization
+	void Start () {
+		// Set the random seed so it's not the same each game.
+		Random.seed = System.DateTime.Today.Millisecond;
+		//TODO get size by script
+		Bounds screenBounds = transform.renderer.bounds;
+		Bounds ballBounds = pref.renderer.bounds;
+		_posY = screenBounds.max.y + ballBounds.size.y;
+		_leftSpawnPosX = screenBounds.min.x + ballBounds.size.x / 2;
+		_rightSpawnPosX = screenBounds.max.x - ballBounds.size.x / 2;
+		
+		// Start the Spawn coroutine.
+		StartCoroutine("AddBall");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator AddBall() {
+		// Create a random wait time before the prop is instantiated.
+		float waitTime = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+		
+		// Wait for the designated period.
+		yield return new WaitForSeconds(waitTime);
+
+		float posX = Random.Range (_leftSpawnPosX, _rightSpawnPosX);
+		Instantiate (pref, new Vector3 (posX, _posY, 0), Quaternion.identity);
+
+
+		StartCoroutine(AddBall());
+	}
+}
